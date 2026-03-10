24687224a2adb65b64571ac74a40398d2a7c32b1
Riya315 <31bhargavariya@gmail.com>
2026-03-10 15:00:48 +0530

    UC1:  Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class BookMyStayApp {
  public static void main(String[] args) {
-
+  System.out.println("======================================");
+  System.out.println("   Welcome to the Hotel Booking App   ");
+  System.out.println("   Application: Book My Stay v1.0     ");
+  System.out.println("======================================");
+  System.out.println("Thank you for using our service!");
  }
+
+
 }
+
+
+
+
+
+
